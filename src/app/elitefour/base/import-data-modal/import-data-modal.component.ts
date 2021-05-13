29249d2991cffcb01308c96f8d2cd496fa04eb20
Rico Apon <rico.apon@gmail.com>
2021-05-13 22:37:24 +0200

    Return to main page when new data is imported

--- src/app/elitefour/base/import-data-modal/import-data-modal.component.ts
+++ src/app/elitefour/base/import-data-modal/import-data-modal.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import {FavoriteListApi} from '../../backend/favorite-list-api';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-import-data-modal',
@@ -34,7 +35,8 @@ export class ImportDataModalComponent implements OnInit {
   importedData: string;
 
   constructor(public activeModal: NgbActiveModal,
-              private favoriteListApi: FavoriteListApi) { }
+              private favoriteListApi: FavoriteListApi,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -42,6 +44,7 @@ export class ImportDataModalComponent implements OnInit {
   import(): void {
     this.favoriteListApi.importFromString(this.importedData);
     this.activeModal.close();
+    this.router.navigate(['/']);
   }
 
   handleFileInput(files: FileList): void {
