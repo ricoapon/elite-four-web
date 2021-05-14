3822de6dfc7ecc6a8049ff00cdd35489f63f90bd
Rico Apon <rico.apon@gmail.com>
2021-05-14 10:37:43 +0200

    Create separate shell script for building app for GitHub Pages

---
+++ build-for-gh-pages.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Determine the version that we implement. We always want the tag as the version. If for some reason we don't have a
+# tag, the --always will make sure that we output the commit hash. This way, we always have an output that is tracable.
+gitVersion=`git describe --tags --always`
+
+# Write the version to the Angular file.
+echo "export const VERSION = '${gitVersion}';" > src/environments/version.ts
+
+ng build --prod
+
+# GitHub Pages needs extra files to make it work correctly:
+# * 404.html, so that all URLs actually route to the application.
+# * .nojekyll, so that GitHub Pages doesn't use the auto configured Jekyll.
+cp dist/elite-four-web/index.html dist/elite-four-web/404.html
+touch dist/elite-four-web/.nojekyll
