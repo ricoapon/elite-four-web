bac64210e41b22696e9cbdc726c8d5bc46425f05
Rico Apon <rico.apon@gmail.com>
2021-05-31 22:24:07 +0200

    Change implementation to extend base class

--- src/app/elitefour/backend/favorite-list-repository-impl.service.ts
+++ src/app/elitefour/backend/favorite-list-repository-impl.service.ts
@@ -1,122 +1,72 @@
-import {FavoriteItem, FavoriteList, FavoriteListStatus} from './favorite-list-interfaces';
-import {Observable, ReplaySubject} from 'rxjs';
+import {FavoriteList, FavoriteListsRepository, FavoriteListStatus} from './favorite-list-interfaces';
+import {BehaviorSubject, Observable} from 'rxjs';
 import {Injectable} from '@angular/core';
-import {FavoriteListStore} from './favorite-list-store';
 
+/**
+ * Implementation of {@link FavoriteListsRepository} that uses localStorage to store the object.
+ */
 @Injectable({
   providedIn: 'root'
 })
-export class FavoriteListsRepositoryImpl {
-  private readonly favoriteListsStorage: FavoriteListStore;
-  private readonly favoriteListsSubject: ReplaySubject<FavoriteList[]>;
+export class FavoriteListsRepositoryImpl extends FavoriteListsRepository {
+  static readonly LOCALSTORAGE_KEY = 'store';
+
+  /** Data that should always be saved to storage when modified. Whenever modified, an event must be pushed favoriteListsSubject. */
+  private favoriteLists: FavoriteList[];
+  private readonly favoriteListsSubject: BehaviorSubject<FavoriteList[]>;
 
   constructor() {
-    this.favoriteListsSubject = new ReplaySubject<FavoriteList[]>(1);
-    this.favoriteListsStorage = new FavoriteListStore(this.favoriteListsSubject);
-  }
-
-  private findListById(listId: number): FavoriteList {
-    return this.favoriteListsStorage.get().find(favoriteList => favoriteList.id === listId);
-  }
-
-  private findItemById(listId: number, itemId: number): FavoriteItem {
-    return this.findListById(listId).items.find(item => item.id === itemId);
-  }
-
-
-
-  updateFavoriteList(favoriteList: FavoriteList): void {
-    const favoriteListInlist: FavoriteList = this.favoriteListsStorage.get().find(list => list.id === favoriteList.id);
-
-    if (!favoriteListInlist) {
-      throw new Error('List ' + favoriteList.id + ' does not exist. Contact administrator.');
+    super();
+    // If we have favoriteLists in storage, retrieve it. Otherwise, initialize with an empty list.
+    if (localStorage.getItem(FavoriteListsRepositoryImpl.LOCALSTORAGE_KEY) !== null) {
+      this.favoriteLists = JSON.parse(localStorage.getItem(FavoriteListsRepositoryImpl.LOCALSTORAGE_KEY));
+    } else {
+      this.favoriteLists = [];
+      this.saveDataToLocalStorage();
     }
 
-    const listIndex = this.favoriteListsStorage.get().indexOf(favoriteListInlist);
-    this.favoriteListsStorage.modify(favoriteLists => favoriteLists[listIndex] = favoriteList);
+    this.favoriteListsSubject = new BehaviorSubject<FavoriteList[]>(this.favoriteLists);
   }
 
   getFavoriteLists(): Observable<FavoriteList[]> {
     return this.favoriteListsSubject.asObservable();
   }
 
-  getFavoriteListById(listId: number): Observable<FavoriteList> {
-    return new Observable<FavoriteList>((observer) => {
-      this.getFavoriteLists().subscribe((favoriteLists) => {
-          observer.next(favoriteLists.find(favoriteList => favoriteList.id === listId));
-      }
-      );
-    });
+  modify(modifier: (favoriteLists: FavoriteList[]) => void): void {
+    modifier(this.favoriteLists);
+    this.saveDataToLocalStorage();
+    this.favoriteListsSubject.next(this.favoriteLists);
   }
 
-  addFavoriteList(listName: string, nrOfItemsToBeShownOnScreen: number): void {
-    const nameExists: boolean = !!this.favoriteListsStorage.get().find(x => x.name === listName);
-
-    if (nameExists) {
-      throw new Error('List with the same name already exists');
-    }
-
-    const favoriteList: FavoriteList = this.favoriteListsStorage.createNewList(listName, nrOfItemsToBeShownOnScreen);
-
-    this.favoriteListsStorage.modify(favoriteLists => favoriteLists.push(favoriteList));
-  }
-
-  deleteFavoriteList(listId: number): void {
-    const favoriteList = this.findListById(listId);
-    this.favoriteListsStorage.modify(favoriteLists => favoriteLists.splice(favoriteLists.indexOf(favoriteList), 1));
-  }
-
-  addItemToFavoriteList(listId: number, itemName: string): void {
-    const favoriteList: FavoriteList = this.findListById(listId);
-    const itemExists: boolean = !!favoriteList.items.find(item => item.name === itemName);
-
-    if (itemExists) {
-      throw new Error('Item with the same name already exists');
-    }
-
-    this.favoriteListsStorage.modify(() => favoriteList.items.push(this.favoriteListsStorage.createNewItem(favoriteList, itemName)));
-  }
-
-  updateItemForFavoriteList(listId: number, updatedItem: FavoriteItem): void {
-    const favoriteList: FavoriteList = this.findListById(listId);
-    const itemInList: FavoriteItem = favoriteList.items.find(item => item.id === updatedItem.id);
-
-    if (!itemInList) {
-      throw new Error('Item ' + updatedItem.id + ' does not exist. Contact administrator.');
-    }
-
-    const itemIndex = favoriteList.items.indexOf(itemInList);
-    this.favoriteListsStorage.modify(() => favoriteList.items[itemIndex] = updatedItem);
-  }
-
-  deleteItemFromFavoriteList(listId: number, itemId: number): void {
-    const items = this.findListById(listId).items;
-    const favoriteItem: FavoriteItem = this.findItemById(listId, itemId);
-    this.favoriteListsStorage.modify(() => items.splice(items.indexOf(favoriteItem), 1));
+  private saveDataToLocalStorage(): void {
+    localStorage.setItem(FavoriteListsRepositoryImpl.LOCALSTORAGE_KEY, JSON.stringify(this.favoriteLists));
   }
 
   resetAlgorithm(listId: number): void {
-    const favoriteList: FavoriteList = this.findListById(listId);
-    favoriteList.status = FavoriteListStatus.CREATED;
-    this.favoriteListsStorage.modify(() => favoriteList.items.forEach((item) => {
-      item.favoritePosition = undefined;
-      item.eliminatedBy = [];
-      item.toBeChosen = false;
-    }));
+    this.modify((favoriteLists: FavoriteList[]) => {
+      const favoriteList: FavoriteList = favoriteLists.find(x => x.id === listId);
+      favoriteList.status = FavoriteListStatus.CREATED;
+      favoriteList.items.forEach((item) => {
+        item.favoritePosition = undefined;
+        item.eliminatedBy = [];
+        item.toBeChosen = false;
+      });
+    });
   }
-
-  removeAllItems(listId: number): void {
-    const favoriteList: FavoriteList = this.findListById(listId);
-    this.favoriteListsStorage.modify(() => favoriteList.items = []);
-  }
-
-
 
   getAsString(): string {
-    return this.favoriteListsStorage.getAsString();
+    return JSON.stringify(this.favoriteLists);
   }
 
   importFromString(data: string): boolean {
-    return this.favoriteListsStorage.saveFromString(data);
+    try {
+      this.favoriteLists = JSON.parse(data);
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+
+    this.saveDataToLocalStorage();
+    return true;
   }
 }
