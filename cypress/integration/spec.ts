6ced157be0c2e85ab92cf571045a29bcaa8c14aa
Rico Apon <rico.apon@gmail.com>
2021-06-05 18:32:52 +0200

    Add first test to try it out (revert this)

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,4 +1,14 @@
-it('loads examples', () => {
+it('Create a new list', () => {
   cy.visit('/');
-  cy.contains('Replace me with something relevant');
+  // Click 'New' button.
+  // cy.get('app-content-header-button > .btn').click();
+  cy.getByTestId('new').click();
+  // Type in the name and add the list.
+  cy.get('#listName').type('New list name');
+  cy.get('.modal-footer > .btn-primary').click();
+  // Open the list.
+  cy.get('.d-flex > .btn-primary').click();
+
+  // We should now see the name of the list on screen.
+  cy.contains('New list name');
 });
