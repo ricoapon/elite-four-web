b71543200e729b31efd5156f8d9e819d07ab7aab
Rico Apon <rico.apon@gmail.com>
2022-12-27 14:09:20 +0100

    Implement Spotify track searching

---
+++ src/app/elitefour/backend/spotify/spotify-search.ts
@@ -0,0 +1,59 @@
+import {Injectable} from '@angular/core';
+import {SpotifyStorage} from './spotify-storage';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+export type Track = {
+  name: string,
+  artist: string,
+  id: string,
+  externalUrl: string,
+};
+
+@Injectable({providedIn: 'root'})
+export class SpotifySearch {
+  static readonly SPOTIFY_SEARCH_URL = 'https://api.spotify.com/v1/search';
+
+  constructor(private spotifyStorage: SpotifyStorage, private httpClient: HttpClient) {}
+
+  searchTracks(searchInput: string): Promise<Track[]> {
+    const paramsString = new HttpParams({
+      fromObject: {
+        type: 'track',
+        // We expect the search input to be complete, so we will only need a few results.
+        limit: '3',
+        q: searchInput,
+      }
+    }).toString();
+
+    return new Promise((resolve, fatal) => {
+      this.executeGetRequest(SpotifySearch.SPOTIFY_SEARCH_URL + '?' + paramsString).subscribe(
+        (response) => {
+          resolve(this.parseResponse(response));
+        },
+        (error) => {
+          fatal(error);
+        }
+      );
+    });
+  }
+
+  private executeGetRequest(url: string): Observable<any> {
+    return this.httpClient.get(url, {
+      headers: new HttpHeaders()
+        .set('Authorization', 'Bearer ' + this.spotifyStorage.getAccessToken())
+    });
+  }
+
+  private parseResponse(response: any): Track[] {
+    const tracks: Track[] = [];
+    for (const track of response.tracks.items) {
+      const name = track.name;
+      const artist = track.artists[0].name;
+      const id = track.id;
+      const externalUrl = track.external_urls.spotify;
+      tracks.push({name, artist, id, externalUrl});
+    }
+    return tracks;
+  }
+}
