9d13676aa87e986656e2f64a562de021514f8ad0
Rico Apon <rico.apon@gmail.com>
2024-08-03 10:04:35 +0200

    Fix running tests

--- src/test.ts
+++ src/test.ts
@@ -1,14 +1,18 @@
+// For some reason, after upgrading to 18, this file must exist but be empty. If you use the content of this file, it will give this error:
+// "Expected to be running in 'ProxyZone', but it was not found."
+// I couldn't figure it out, so I guess I will leave it like this until I understand the problem.
+
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/dist/zone-testing';
-import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
-
-// First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
-);
+// import 'zone.js/testing';
+// import { getTestBed } from '@angular/core/testing';
+// import {
+//   BrowserDynamicTestingModule,
+//   platformBrowserDynamicTesting
+// } from '@angular/platform-browser-dynamic/testing';
+//
+// // First, initialize the Angular testing environment.
+// getTestBed().initTestEnvironment(
+//   BrowserDynamicTestingModule,
+//   platformBrowserDynamicTesting()
+// );
