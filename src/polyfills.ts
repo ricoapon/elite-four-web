76ce026215fff272722dce4d04b702b6ca916039
Rico Apon <rico.apon@gmail.com>
2024-08-03 09:14:31 +0200

    Update to Angular 17

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,8 +61,8 @@ import '@angular/localize/init';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+import 'zone.js';
+import 'zone.js/testing';
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
