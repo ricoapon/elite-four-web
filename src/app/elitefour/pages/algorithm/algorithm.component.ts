ccefafde37e76444ee50e29cbdebd03aa763a77c
Rico Apon <rico.apon@gmail.com>
2026-06-16 08:39:54 +0000

    Use dynamic columns when picking items

--- src/app/elitefour/pages/algorithm/algorithm.component.ts
+++ src/app/elitefour/pages/algorithm/algorithm.component.ts
@@ -4,16 +4,34 @@ import {FavoritePickerAlgorithm} from '../../backend/favorite-picker-algorithm.s
 import {FavoriteItem, FavoriteList, FavoriteListStatus} from '../../backend/favorite-list-interfaces';
 import {FavoriteListsRepository} from '../../backend/favorite-lists-repository';
 
+type PickerColumnLayout = 'auto' | '2' | '3' | '4' | '5';
+
+type PickerColumnOption = {
+  value: PickerColumnLayout,
+  label: string,
+  ariaLabel: string
+};
+
 @Component({
   selector: 'app-algorithm',
   templateUrl: './algorithm.component.html',
   styleUrls: ['./algorithm.component.css']
 })
 export class AlgorithmComponent implements OnInit {
+  private static readonly PICKER_COLUMN_LAYOUT_STORAGE_KEY = 'elite-four-picker-column-layout';
+
   favoriteList: FavoriteList;
   toBeChosenItems: FavoriteItem[];
   selectedItems: FavoriteItem[];
   newFavorites: FavoriteItem[] = [];
+  pickerColumnLayout: PickerColumnLayout = 'auto';
+  readonly pickerColumnOptions: PickerColumnOption[] = [
+    {value: 'auto', label: 'Auto', ariaLabel: 'Use automatic picker columns'},
+    {value: '2', label: '2', ariaLabel: 'Use two picker columns'},
+    {value: '3', label: '3', ariaLabel: 'Use three picker columns'},
+    {value: '4', label: '4', ariaLabel: 'Use four picker columns'},
+    {value: '5', label: '5', ariaLabel: 'Use five picker columns'}
+  ];
   private algorithm: FavoritePickerAlgorithm;
 
   constructor(private route: ActivatedRoute,
@@ -22,6 +40,8 @@ export class AlgorithmComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.pickerColumnLayout = this.loadPickerColumnLayout();
+
     const listId = +this.route.snapshot.paramMap.get('id');
     this.algorithm = new FavoritePickerAlgorithm(this.favoriteListsRepository, listId);
     this.favoriteListsRepository.getFavoriteListById(listId).subscribe((list) => this.favoriteList = list);
@@ -73,5 +93,20 @@ export class AlgorithmComponent implements OnInit {
     const nrOfFavoriteItemsPicked = this.favoriteList.items.filter((item) => !!item.favoritePosition).length;
 
     return 100 * (nrOfFavoriteItemsPicked / this.favoriteList.items.length);
+  }
+
+  setPickerColumnLayout(pickerColumnLayout: PickerColumnLayout): void {
+    this.pickerColumnLayout = pickerColumnLayout;
+    localStorage.setItem(AlgorithmComponent.PICKER_COLUMN_LAYOUT_STORAGE_KEY, pickerColumnLayout);
+  }
+
+  private loadPickerColumnLayout(): PickerColumnLayout {
+    const storedPickerColumnLayout = localStorage.getItem(AlgorithmComponent.PICKER_COLUMN_LAYOUT_STORAGE_KEY);
+
+    return this.isPickerColumnLayout(storedPickerColumnLayout) ? storedPickerColumnLayout : 'auto';
+  }
+
+  private isPickerColumnLayout(value: string | null): value is PickerColumnLayout {
+    return this.pickerColumnOptions.some((option) => option.value === value);
   }
 }
