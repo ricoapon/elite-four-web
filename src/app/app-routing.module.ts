e63411b08975733fe9f0575e0b58d633b437ef66
Rico Apon <rico.apon@gmail.com>
2026-06-16 14:48:10 +0000

    Create page for debugging search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {AlgorithmComponent, HelpComponent, ListDetailComponent, ListOverviewComponent, PageNotFoundComponent} from './elitefour/pages';
 import {SpotifyCallbackComponent} from './elitefour/pages/spotify-callback/spotify-callback.component';
 import {IntegrationTestComponent} from './elitefour/pages/integration-test/integration-test.component';
+import {SpotifySearchDebugComponent} from './elitefour/pages/spotify-search-debug/spotify-search-debug.component';
 
 const routes: Routes = [
   {path: '', component: ListOverviewComponent},
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path: 'help', component: HelpComponent},
   {path: 'spotify-callback', component: SpotifyCallbackComponent},
   {path: 'integration-test', component: IntegrationTestComponent},
+  {path: 'spotify-search-debug', component: SpotifySearchDebugComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
 
